3853e7c4668ec57521f9f2bfdbeeccf936cf2a45
Will <willforan@gmail.com>
2011-12-19 10:33:59 -0500

    Update output to be %Theta Cycle

--- spikestats.m
+++ spikestats.m
@@ -3,13 +3,20 @@ function spikestats
 
   for p=1:P
       spikeTimes = pyramidal(p).spikeTimes;
-      phase	 = mod(spikeTimes,125);
+      phase	 = mod(spikeTimes,125)./125;
+      cycle	 = ceil(spikeTimes./125);
       ISI        = spikeTimes(2:end) - spikeTimes(1:end-1);
 
-      fprintf('Cell\t%i\n',p);
-      fprintf('time\t');	disp(spikeTimes);
-      fprintf('ISI\t');		disp(ISI);
-      fprintf('Phase\t');	disp(phase);
+      indent='	';
+      for i=1:min(cycle)
+	indent=[indent '	'];
+      end
+
+      fprintf('\nCell\t%i\n',p);
+      fprintf('spiketime(ms)       %s',indent);		fprintf('%i\t',spikeTimes);	fprintf('\n');
+      fprintf('ISI(ms)             \t%s',indent);	fprintf('%i\t',ISI);		fprintf('\n');
+      fprintf('Phase(percent cycle)%s',indent);		fprintf('%.2f\t',phase); 	fprintf('\n');
+      fprintf('Theta cycle         %s',indent);		fprintf('%i\t',cycle);	 	fprintf('\n');
   end
 
 end
